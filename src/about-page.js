e4e7ffa9908d9d90946ac4eef51d00abd5f7f907
oscarnava <oscarnava@gmail.com>
2019-11-01 15:23:08 -0600

    Add contacts page

--- src/about-page.js
+++ src/about-page.js
@@ -3,15 +3,21 @@ import updateContents from './updateContents';
 let contents = null;
 
 function createContents() {
-  const title = document.createElement('h1');
-  title.innerText = "Malola's Restaurant";
+  const aboutCtnr = document.createElement('div');
+  aboutCtnr.id = 'about-container';
+
+  // const title = document.createElement('h1');
+  // title.innerText = "Malola's Restaurant";
 
   const subtitle = document.createElement('h2');
   subtitle.innerText = 'The best food in town! Sopa tarasca, Corundas, Enchiladas morelianas o placeras, Morisqueta, Carnitas, Aporreadillo, Uchepos, Chongos zamoranos, Ate, Nieve de pasta and much more...';
 
+  // aboutCtnr.appendChild(title);
+  aboutCtnr.appendChild(subtitle);
+
   console.log('About page contents created');
 
-  return [title, subtitle];
+  return [aboutCtnr];
 }
 
 export default function populateAboutPage() {
--- src/index.js
+++ src/index.js
@@ -5,18 +5,26 @@ import populateContactPage from './contact-page';
 
 const qrySelect = document.querySelector.bind(document);
 const mainContainer = qrySelect('#content');
+let currentPage;
 
-function swapContents(populateFn) {
-  return () => fadeSwap(mainContainer, populateFn, 42);
+function swapContents(populateFn, page) {
+  return () => {
+    if (page !== currentPage) {
+      fadeSwap(mainContainer, populateFn, 42);
+      currentPage = page;
+    }
+  };
 }
 
-function setMenuItemEventListener(selector, contentsFn) {
-  qrySelect(selector).addEventListener('click', swapContents(contentsFn));
+function setMenuItemEventListener(selector, contentsFn, page) {
+  qrySelect(selector).addEventListener('click', swapContents(contentsFn, page));
 }
 
 populateAboutPage();
+currentPage = 1;
+
 fadeIn(document.getElementsByTagName('body')[0], 50);
 
-setMenuItemEventListener('#about', populateAboutPage);
-setMenuItemEventListener('#dishes', populateDishesPage);
-setMenuItemEventListener('#contact', populateContactPage);
+setMenuItemEventListener('#about', populateAboutPage, 1);
+setMenuItemEventListener('#dishes', populateDishesPage, 2);
+setMenuItemEventListener('#contact', populateContactPage, 3);
